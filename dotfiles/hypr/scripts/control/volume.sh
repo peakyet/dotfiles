75b83166235c5a428484dcb333a26dea9c9c4ff7
Weifeng Zeng <13025291859@163.com>
2023-09-23 14:42:29 +0800

    feat: add icons for notification

--- dotfiles/hypr/scripts/control/volume.sh
+++ dotfiles/hypr/scripts/control/volume.sh
@@ -2,6 +2,9 @@
 
 msgTag="myVolume"
 
+# Query the volume before changing it
+volumeBefore="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')"
+
 amixer set Master $1
 
 # Query amixer for the current volume and whether or not the speaker is muted
@@ -11,10 +14,15 @@ mute="$(amixer -c 0 get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g'
 
 if [[ $volume == 0 || "$mute" == "off" ]]; then
     # Show the sound muted notification
-    dunstify -a "Volume" -u low -i /usr/share/icons/Adwaita/16x16/status/audio-volume-muted-symbolic.symbolic.png -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Status: $mute, Value: " 
+    dunstify -a "Volume" -u low -i $HOME/.config/dunst/imgs/volume/volume-off.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Status: $mute, Value: " 
 else
     # Show the volume notification
-    dunstify -a "Volume" -u low -i /usr/share/icons/Adwaita/16x16/status/audio-volume-high-symbolic.symbolic.png -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Status: $mute, Value: "
+    if [ $volumeBefore -lt $volume ]
+    then
+	    dunstify -a "Volume" -u low -i $HOME/.config/dunst/imgs/volume/volume-plus.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Status: $mute, Value: "
+    else
+	    dunstify -a "Volume" -u low -i $HOME/.config/dunst/imgs/volume/volume-minus.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Status: $mute, Value: "
+    fi
 fi
 
 # Play the volume changed sound
