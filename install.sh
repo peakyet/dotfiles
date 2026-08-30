f722437eed405a80023d1c8c8264804b84c27627
Weifeng Zeng <13025291859@163.com>
2026-08-30 16:49:53 +0800

    refactor: DSH plugin

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ allDotfiles=$(ls $HOME/dotfiles/dotfiles)
 path="$(pwd)/dotfiles"
 excludeConfigs=(
 	# "alacritty"
-	# "bob"
 	# "bashrc"
 	# "cava"
 	# "gitconfig"
 	# "helix"
 	# "hypr"
-	"DSH-Plugins"
+	# "DSH-Plugins"
 	"joshuto"
 	# "kitty"
 	# "ncmpcpp"
@@ -26,8 +25,6 @@ excludeConfigs=(
 	# "zsh"
 	# "zshrc"
 	# "niri"
-	"zshrc_omz"
-	"ClaudeCode"
 	"readme.md"
 	"README.md"
 )
@@ -53,72 +50,75 @@ linkFile(){
 	fi
 }
 
-linkDotfiles(){
-        # link all dotfiles in this repo
-        # args: allDotfiles, whether to remove the exist file or linksymbol.
-        echo "start linking dotfiles ..."
-        for file in $1
-        do
-					# check if the file is in the exclude list
-					if [[ " ${excludeConfigs[@]} " =~ " ${file} " ]]
-					then
-						echo "exclude ${file}"
-					 	echo "-------------------------"
-					 	continue
-					fi
+linkDSHPlugins(){
+	dshProfileDir="$HOME/.dsh/profiles/web"
+	echo "start linking dsh plugin to $dshProfileDir"
+	if [ ! -d "$dshProfileDir" ]
+	then
+		echo "create $dshProfileDir"
+		mkdir -p "$dshProfileDir"
+	fi
+	for file in $1
+	do
+		if [[ $file = "readme.md" || $file = "README.md" ]]; then
+			continue
+		fi
 
-					sourcefile="$2/$file"
-					if [ -d $sourcefile ]
-					then
-						if [ $file = "zsh" ]
-						then
-							filesInFolder=$(ls "$sourcefile")
-							linkDotfiles "$filesInFolder" $sourcefile $3
-						else
-							toFile="$HOME/.config/${file}"
-							linkFile $sourcefile $toFile $3
-						fi
-          else
-						if [ $file = "zshrc_omz" ]
-						then
-							toFile="$HOME/.zshrc"
-						else
-							toFile="$HOME/.${file}"
-						fi
-						linkFile $sourcefile $toFile $3
-          fi
-					echo "-------------------------"
-	    	done
+		sourceFile="$2/$file"
+		toFile="$dshProfileDir/$file"
+		linkFile $sourceFile $toFile $3
+	done
+}
+
+linkDotfiles(){
+	# link all dotfiles in this repo
+	# args: allDotfiles, whether to remove the exist file or linksymbol.
+	echo "start linking dotfiles ..."
+	for file in $1
+	do
+		# check if the file is in the exclude list
+		if [[ " ${excludeConfigs[@]} " =~ " ${file} " ]]
+		then
+			echo "exclude ${file}"
+			echo "-------------------------"
+			continue
+		fi
+
+		filePath="$2/$file"
+		if [ -d $filePath ]
+		then
+			if [[ $file = "zsh" ]]; then
+				filesInFolder=$(ls "$filePath")
+				linkDotfiles "$filesInFolder" $filePath $3
+			elif [[ $file = "DSH-Plugins" ]]; then
+				filesInFolder=$(ls "$filePath")
+				linkDSHPlugins "$filesInFolder" $filePath $3
+			elif [[ $file = "agents" ]]; then
+				toFile="$HOME/.${file}"
+				linkFile $filePath $toFile $3
+			else
+				toFile="$HOME/.config/${file}"
+				linkFile $filePath $toFile $3
+			fi
+		else
+			toFile="$HOME/.${file}"
+			linkFile $filePath $toFile $3
+		fi
+		echo "-------------------------"
+	done
 }
 
 if [ $1 ]
 then
-  if [ $1 = "force" ]
-  then
-    linkDotfiles "$allDotfiles" $path true
+	if [ $1 = "force" ]
+	then
+		linkDotfiles "$allDotfiles" $path true
 	else
 		echo "Invalid argument: $1"
-  fi
+	fi
 else
-  linkDotfiles "$allDotfiles" $path
+	linkDotfiles "$allDotfiles" $path
 fi
-
-# link the dsh desktop-notify plugin into the dsh web profile so it can be
-# loaded via ~/.dsh/profiles/web/cordis.patch.yml (name: './dsh-desktop-notify.js')
-dshProfileDir="$HOME/.dsh/profiles/web"
-echo "start linking dsh desktop-notify plugin to $dshProfileDir"
-if [ ! -d "$dshProfileDir" ]
-then
-	echo "create $dshProfileDir"
-	mkdir -p "$dshProfileDir"
-fi
-if [ "$1" = "force" ]
-then
-	linkFile "$path/DSH-Plugins/dsh-desktop-notify/notify.js" "$dshProfileDir/dsh-desktop-notify.js" true
-else
-	linkFile "$path/DSH-Plugins/dsh-desktop-notify/notify.js" "$dshProfileDir/dsh-desktop-notify.js"
-fi
-echo "-------------------------"
 
 # set service of niri
 # source "${path}/niri/add_wants.sh"
